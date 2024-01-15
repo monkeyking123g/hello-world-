78ed413c7ab25c721952c51784b07e7dff54ec64
Dmytro Ipatii <87619572+monkeyking123g@users.noreply.github.com>
2024-01-15 16:39:08 +0000

    dd

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const test = 'dima';
+  const test = 'dima change';
   const app = await NestFactory.create(AppModule);
   await app.listen(3000);
 }
